7a9ca92e98ade297d443a8cbdb7e948180c7251c
GabrielVRM <gabriel.vieira2595@gmail.com>
2023-11-30 17:29:05 -0300

    add html com JS

--- Promisses/js/Favorites.js
+++ Promisses/js/Favorites.js
@@ -1,30 +1,74 @@
-// Classe que vai conter logica dos dados 
+// Classe que vai conter logica dos dados
 
- export class Favorites {
-    constructor (root){
-        this.root = document.querySelector(root)
-    }
+export class Favorites {
+  constructor(root) {
+    this.root = document.querySelector(root);
+    this.load()
+  }
+
+  load(){
+    this.entries = [
+        {
+          login: "GabrielVRM",
+          name: "Gabriel Vieira",
+          public_repos: "87",
+          followers: "120000",
+        },
+        {
+          login: "Diegogb",
+          name: "Diego Reis",
+          public_repos: "100",
+          followers: "12000",
+        },
+      ]
+  }
 }
-
 
 // classe que vai criar visualização e eventos html
 export class FavoritesView extends Favorites {
-    constructor(root){
-        super(root)
-        console.log(this.root)
-this.update()
-      
-    }
-    update(){
-          this.removeAllTr()
-    }
+  constructor(root) {
+    super(root);
+    console.log(this.root);
+    this.update();
+  }
+  update() {
+    // this.removeAllTr()
+  
+    this.entries.forEach( user => {
+const row = this.createRow()
+console.log(row)
+    })
+  }
 
-    removeAllTr(){
-        const tbody = this.root.querySelector('table tbody')
+  createRow() {
+    const tr = document.createElement("tr");
+    const data = `
+            <td class="user">
+            <img src="https://github.com/maykbrito.png" alt="Imagem de Mayk Brito">
+            <a href="https://github.com/maykbrito" target="_blank">
+            <p>Mayk Brito</p>
+            <span>maykbrito</span>
+                </a>
+                </td>
+                <td class="repositories">
+                76
+                </td>
+                <td class="followers">
+                9589
+                </td>
+                <td>&times;</td>  
+                `;
 
-        tbody.querySelectorAll('tr')
-        .forEach( tr => {
-            tr.remove()
-        })
-    }
+    tr.innerText = data;
+
+    return tr;
+  }
+
+  removeAllTr() {
+    const tbody = this.root.querySelector("table tbody");
+
+    tbody.querySelectorAll("tr").forEach((tr) => {
+      tr.remove();
+    });
+  }
 }
